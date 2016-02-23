f2eea40c95b582972f7c7d349f000db00b7b6f59
fancyboynet <fancyboynet@gmail.com>
2016-02-23 18:01:30 +0800

    增加示例组件

---
+++ components/app/red/red.js
@@ -0,0 +1,11 @@
+/**
+ * red组件
+ * @require red.css
+ * @module app/red/red
+ * @requires jquery/jquery
+ * @author fancy
+ */
+var $ = require('jquery/jquery');
+module.exports = function(el){
+	$(el).addClass('modTest');
+};
--- page/template/template.js
+++ page/template/template.js
@@ -1,4 +1,7 @@
 var $ = require('jquery/jquery');
+var red = require('app/red/red');
+
 $(function(){
     $('img').attr('src', $('img').attr('data-src'));
+    red($('h1'));
 });
